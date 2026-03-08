500fe253ac365c91144b888e741c53acd88494af
argosdidit5963 <120153198+argosdidit@users.noreply.github.com>
2026-03-08 23:54:30 +0900

    Update SearchAbilityDex.js

--- SearchAbilityDex/SearchAbilityDex.js
+++ SearchAbilityDex/SearchAbilityDex.js
@@ -89,7 +89,7 @@ const PokeSearch = (() => {
         <div class="reload-button">
         <button id="BtnReload">
         <a id="decisionButton" href="NewPokedex.html">
-        <img src="image/roll.jpeg" height="30px" width="30px">
+        <img src="../image/roll.jpeg" height="30px" width="30px">
         </a>
         </button>
         </div>
@@ -472,4 +472,5 @@ const PokeSearch = (() => {
 
 window.addEventListener('load', function(){
   PokeSearch();
+
 });
