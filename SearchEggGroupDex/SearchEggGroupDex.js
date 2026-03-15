dcf29047a2f07c7894ce4ef7b604334630ee8561
argosdidit5963 <120153198+argosdidit@users.noreply.github.com>
2026-03-15 20:19:16 +0900

    Add files via upload

--- SearchEggGroupDex/SearchEggGroupDex.js
+++ SearchEggGroupDex/SearchEggGroupDex.js
@@ -34,9 +34,9 @@ const PokeSearch = (() => {
       flag = true;
       areaSidebar = document.querySelector(`[${conf.area_sidebar}]`);
 
-      jap_or_eng = localStorage.getItem("selectedLanguage") || 'jap';
-      bright_or_dark = localStorage.getItem("selectedWindow") || 'bright';
-      normal_or_shiny = localStorage.getItem("selectedImage") || 'normal';
+      jap_or_eng = localStorage.getItem("selectedLanguage") || 'JAP';
+      bright_or_dark = localStorage.getItem("selectedWindow") || 'BRIGHT';
+      normal_or_shiny = localStorage.getItem("selectedImage") || 'NORMAL';
 
       return this;
     },
@@ -68,9 +68,9 @@ const PokeSearch = (() => {
         const selectedBtnImage = document.getElementById('BtnImage');
 
         // ★ localStorage から読んだ状態をチェックに反映
-        selectedBtnLanguage.checked = (jap_or_eng === 'eng');
-        selectedBtnWindow.checked   = (bright_or_dark === 'dark');
-        selectedBtnImage.checked    = (normal_or_shiny === 'shiny');
+        selectedBtnLanguage.checked = (jap_or_eng === 'ENG');
+        selectedBtnWindow.checked   = (bright_or_dark === 'DARK');
+        selectedBtnImage.checked    = (normal_or_shiny === 'SHINY');
         
         // ★ ラベルも一度現在状態で更新
         func.updateLabels();
@@ -103,7 +103,6 @@ const PokeSearch = (() => {
     updateURL: async function(){
       if(flag){
         let newURL = `../SearchEggGroupDex/SearchEggGroupDex.html`;
-        //window.location.href = newURL;
         history.replaceState(null, "", newURL);
       }
       return this;
@@ -118,25 +117,25 @@ const PokeSearch = (() => {
       const lblImage = btnImage.nextElementSibling;
       
       // まず状態を更新
-      jap_or_eng = btnLanguage.checked ? 'eng' : 'jap';
-      bright_or_dark = btnWindow.checked ? 'dark' : 'bright';
-      normal_or_shiny = btnImage.checked ? 'shiny' : 'normal';
+      jap_or_eng = btnLanguage.checked ? 'ENG' : 'JAP';
+      bright_or_dark = btnWindow.checked ? 'DARK' : 'BRIGHT';
+      normal_or_shiny = btnImage.checked ? 'SHINY' : 'NORMAL';
 
       // 言語
-      lblLanguage.textContent = (jap_or_eng === 'jap') ? '日本語' : 'English';
+      lblLanguage.textContent = (jap_or_eng === 'JAP') ? '日本語' : 'English';
       
       // ウィンドウ(明るさ)
-      if (jap_or_eng === 'jap') {
-        lblWindow.textContent = (bright_or_dark === 'bright') ? 'ブライト' : 'ダーク';
+      if (jap_or_eng === 'JAP') {
+        lblWindow.textContent = (bright_or_dark === 'BRIGHT') ? 'ブライト' : 'ダーク';
       } else {
-        lblWindow.textContent = (bright_or_dark === 'bright') ? 'Bright' : 'Dark';
+        lblWindow.textContent = (bright_or_dark === 'BRIGHT') ? 'Bright' : 'Dark';
       }
       
       // 画像(通常/色違い)
-      if (jap_or_eng === 'jap') {
-        lblImage.textContent = (normal_or_shiny === 'normal') ? '通常' : '色違い';
+      if (jap_or_eng === 'JAP') {
+        lblImage.textContent = (normal_or_shiny === 'NORMAL') ? '通常' : '色違い';
       } else {
-        lblImage.textContent = (normal_or_shiny === 'normal') ? 'Normal' : 'Shiny';
+        lblImage.textContent = (normal_or_shiny === 'NORMAL') ? 'Normal' : 'Shiny';
       }
 
       localStorage.setItem('selectedLanguage', jap_or_eng);
@@ -154,7 +153,7 @@ const PokeSearch = (() => {
       }
       return this;
     },
-    makeSidebarArea: function () {
+    makeJapSidebarArea: function () {
       htmlSidebar = `
         <nav class="sidebar" id="sidebar">
           <button class="sidebar-close-btn" id="sidebar-close">☰</button>
@@ -165,6 +164,23 @@ const PokeSearch = (() => {
             <li data-menu="4"><a href="../SearchEggGroupDex/SearchEggGroupDex.html">タマゴグループ</a></li>
             <li data-menu="5"><a href="../SearchValueDex/SearchValueDex.html">種族値</a></li>
             <li data-menu="6"><a href="../SearchFormDex/SearchFormDex.html">姿違い</a></li>
+          </ul>
+        </nav>
+      `;
+      areaSidebar.insertAdjacentHTML('beforeend', htmlSidebar);
+      return this;
+    },
+    makeEngSidebarArea: function () {
+      htmlSidebar = `
+        <nav class="sidebar" id="sidebar">
+          <button class="sidebar-close-btn" id="sidebar-close">☰</button>
+          <ul>
+            <li data-menu="1"><a href="../SearchTypeDex/SearchTypeDex.html">Search Type</a></li>
+            <li data-menu="2"><a href="../SearchAbilityDex/SearchAbilityDex.html">Search Ability</a></li>
+            <li data-menu="3"><a href="../SearchGenderDex/SearchGenderDex.html">Search Gender</a></li>
+            <li data-menu="4"><a href="../SearchEggGroupDex/SearchEggGroupDex.html">Search Egg Group</a></li>
+            <li data-menu="5"><a href="../SearchValueDex/SearchValueDex.html">Search Value</a></li>
+            <li data-menu="6"><a href="../SearchFormDex/SearchFormDex.html">Search Form</a></li>
           </ul>
         </nav>
       `;
@@ -218,14 +234,31 @@ const PokeSearch = (() => {
     },
     initEggGroupTable: function(){
       if(flag){
-        //fetch('http://127.0.0.1:3001/api/poke/egg_group')
-        fetch('/api/poke/egg_group')
-          .then(res => res.json())
-          .then(egg_groups => func.renderEggGroupTable(egg_groups));
+        switch(jap_or_eng)
+        {
+          case 'JAP':
+            //fetch('http://127.0.0.1:3001/api/poke/egg_group')
+            fetch('/api/poke/egg_group')
+            .then(res => res.json())
+            .then(egg_groups => func.renderJapEggGroupTable(egg_groups));
+            break;
+          case 'ENG':
+            //fetch('http://127.0.0.1:3001/api/poke/egg_group')
+            fetch('/api/poke/egg_group')
+            .then(res => res.json())
+            .then(egg_groups => func.renderEngEggGroupTable(egg_groups));
+            break;
+          default:
+            //fetch('http://127.0.0.1:3001/api/poke/egg_group')
+            fetch('/api/poke/egg_group')
+            .then(res => res.json())
+            .then(egg_groups => func.renderJapEggGroupTable(egg_groups));
+            break;
+        }
       }
       return this;
     },
-    renderEggGroupTable: function(egg_groups){
+    renderJapEggGroupTable: function(egg_groups){
       if(flag){
         const area = document.getElementById("TblSearchEggGroup");
         area.innerHTML = "";
@@ -256,16 +289,64 @@ const PokeSearch = (() => {
       }
       return this;
     },
-    initRegionList: function(){
+    renderEngEggGroupTable: function(egg_groups){
       if(flag){
-        //fetch('http://127.0.0.1:3001/api/poke/region')
-        fetch('/api/poke/region')
-          .then(res => res.json())
-          .then(regions => func.renderRegionList(regions));
+        const area = document.getElementById("TblSearchEggGroup");
+        area.innerHTML = "";
+        
+        egg_groups.forEach(g => {
+          const btn = document.createElement("button");
+          btn.type = "button";
+          btn.classList.add("egg_group-btn");
+          btn.textContent = g.egg_group;
+          btn.dataset.egg_groupid = g.egg_groupid;
+          
+          btn.addEventListener("click", () => {
+            const id = Number(btn.dataset.egg_groupid);
+            
+            if (selectedEggGroups.includes(id))
+            {
+              selectedEggGroups = selectedEggGroups.filter(x => x !== id);
+              btn.classList.remove("selected");
+            }
+            else
+            {
+              selectedEggGroups.push(id);
+              btn.classList.add("selected");
+            }
+          });
+          area.appendChild(btn);
+        });
       }
       return this;
     },
-    renderRegionList(regions){
+    initRegionList: function(){
+      if(flag){
+        switch(jap_or_eng)
+        {
+          case 'JAP':
+            //fetch('http://127.0.0.1:3001/api/poke/region')
+            fetch('/api/poke/region')
+            .then(res => res.json())
+            .then(regions => func.renderJapRegionList(regions));
+            break;
+          case 'ENG':
+            //fetch('http://127.0.0.1:3001/api/poke/region')
+            fetch('/api/poke/region')
+            .then(res => res.json())
+            .then(regions => func.renderEngRegionList(regions));
+            break;
+          default:
+            //fetch('http://127.0.0.1:3001/api/poke/region')
+            fetch('/api/poke/region')
+            .then(res => res.json())
+            .then(regions => func.renderJapRegionList(regions));
+            break;
+        }
+      }
+      return this;
+    },
+    renderJapRegionList(regions){
       if(flag){
         const ddlRegion = document.getElementById("DdlSearchRegion");
 
@@ -278,16 +359,46 @@ const PokeSearch = (() => {
       }
       return this;
     },
-    initGenList: function(){
+    renderEngRegionList(regions){
       if(flag){
-        //fetch('http://127.0.0.1:3001/api/poke/generation')
-        fetch('/api/poke/generation')
-          .then(res => res.json())
-          .then(gens => func.renderGenList(gens));
+        const ddlRegion = document.getElementById("DdlSearchRegion");
+
+        regions.forEach(r => {
+          const opt = document.createElement("option");
+          opt.value = r.regionid;
+          opt.textContent = `${r.regionid}: ${r.region}`;
+          ddlRegion.appendChild(opt);
+        });
       }
       return this;
     },
-    renderGenList: function(gens){
+    initGenList: function(){
+      if(flag){
+        switch(jap_or_eng)
+        {
+          case 'JAP':
+            //fetch('http://127.0.0.1:3001/api/poke/generation')
+            fetch('/api/poke/generation')
+            .then(res => res.json())
+            .then(gens => func.renderJapGenList(gens));
+            break;
+          case 'ENG':
+            //fetch('http://127.0.0.1:3001/api/poke/generation')
+            fetch('/api/poke/generation')
+            .then(res => res.json())
+            .then(gens => func.renderEngGenList(gens));
+            break;
+          default:
+            //fetch('http://127.0.0.1:3001/api/poke/generation')
+            fetch('/api/poke/generation')
+            .then(res => res.json())
+            .then(gens => func.renderJapGenList(gens));
+            break;
+        }
+      }
+      return this;
+    },
+    renderJapGenList: function(gens){
       if(flag){
         const ddlGen = document.getElementById("DdlSearchGen");
 
@@ -300,6 +411,19 @@ const PokeSearch = (() => {
       }
       return this;
     },
+    renderEngGenList: function(gens){
+      if(flag){
+        const ddlGen = document.getElementById("DdlSearchGen");
+
+        gens.forEach(g => {
+          const opt = document.createElement("option");
+          opt.value = g.generationid;
+          opt.textContent = `${g.generationid}: ${g.generation}`;
+          ddlGen.appendChild(opt);
+        });
+      }
+      return this;
+    },
     searchEggGroupPoke: function(){
       if(flag){
         const name = document.getElementById("TxtSearchName").value;
@@ -307,18 +431,42 @@ const PokeSearch = (() => {
         const region = document.getElementById("DdlSearchRegion").value;
         const generation = document.getElementById("DdlSearchGen").value;
 
-        //fetch('http://127.0.0.1:3001/api/search/egg_group', {
-        fetch('/api/search/egg_group', {
-          method: "POST",
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify({name, egg_groups, region, generation})
-        })
-        .then(res => res.json())
-        .then(data => func.renderSearchResult(data));
+        switch(jap_or_eng)
+        {
+          case 'JAP':
+            //fetch('http://127.0.0.1:3001/api/search/egg_group', {
+            fetch('/api/search/egg_group', {
+              method: "POST",
+              headers: {"Content-Type": "application/json"},
+              body: JSON.stringify({name, egg_groups, region, generation})
+            })
+            .then(res => res.json())
+            .then(data => func.renderJapSearchResult(data));
+          case 'ENG':
+            //fetch('http://127.0.0.1:3001/api/search/egg_group', {
+            fetch('/api/search/egg_group', {
+              method: "POST",
+              headers: {"Content-Type": "application/json"},
+              body: JSON.stringify({name, egg_groups, region, generation})
+            })
+            .then(res => res.json())
+            .then(data => func.renderEngSearchResult(data));
+            break;
+          default:
+            //fetch('http://127.0.0.1:3001/api/search/egg_group', {
+            fetch('/api/search/egg_group', {
+              method: "POST",
+              headers: {"Content-Type": "application/json"},
+              body: JSON.stringify({name, egg_groups, region, generation})
+            })
+            .then(res => res.json())
+            .then(data => func.renderJapSearchResult(data));
+            break;
+        }
       }
       return this;
     },
-    renderSearchResult: function(list){
+    renderJapSearchResult: function(list){
       if(flag){
         const body = document.getElementById("SearchResultBody");
         body.innerHTML = "";
@@ -340,10 +488,25 @@ const PokeSearch = (() => {
         document.getElementById("LblNoResult").style.display = "none";
         
         list.forEach(p => {
+
+          let displayImage;
+
+          switch(normal_or_shiny)
+          {
+            case 'NORMAL':
+              displayImage = p.PATH_NORMAL_FRONT;
+              break;
+            case 'SHINY':
+              displayImage = p.PATH_SHINY_FRONT;
+              break;
+            default:
+              displayImage = p.PATH_NORMAL_FRONT;
+              break;
+          }
           const tr1 = document.createElement("tr");
           tr1.innerHTML =
           `
-          <td width="5%" rowspan="2"><img src="../${p.path_normal_front}" class="middle-each-image"></td>
+          <td width="5%" rowspan="2"><img src="../${displayImage}" class="middle-each-image"></td>
           <td width="5%" rowspan="2">${p.no}</td>
           <td width="15%">${p.namae}</td>
           <td width="30%" colspan="3">${p.sugata ? p.sugata : ""}</td>
@@ -352,7 +515,84 @@ const PokeSearch = (() => {
           <td width="5%">${p.hp}</td>
           <td width="5%">${p.attack}</td>
           <td width="5%">${p.defense}</td>
-          <td width="5%"><a href="../NewPokedex/NewPokedex.html?poke_autonum=${p.autonum}">リンク</a></td>
+          <td width="5%"><a href="../NewPokedex/NewPokedex.html?poke_AUTONUM=${p.AUTONUM}"
+                            data-lang="${jap_or_eng}"
+                            data-window="${bright_or_dark}"
+                            >リンク</a></td>
+          `;
+          const tr2 = document.createElement("tr");
+          tr2.innerHTML =
+          `
+          <td width="15%">${p.taipu1}${p.taipu2 ? "・" + p.taipu2 : ""}</td>
+          <td width="10%">${p.tokusei1}</td>
+          <td width="10%">${p.tokusei2 ? p.tokusei2 : ""}</td>
+          <td width="10%">${p.yume_tokusei ? p.yume_tokusei : ""}</td>
+          <td>${p.gender}</td>
+          <td>${p.sedai}</td>
+          <td width="5%">${p.sp_atk}</td>
+          <td width="5%">${p.sp_def}</td>
+          <td width="5%">${p.speed}</td>
+          <td width="5%">${p.sum}</td>
+          `;
+          body.appendChild(tr1);
+          body.appendChild(tr2);
+        });
+      }
+      return this;
+    },
+    renderEngSearchResult: function(list){
+      if(flag){
+        const body = document.getElementById("SearchResultBody");
+        body.innerHTML = "";
+        
+        // ★ ここ重要
+        if (!Array.isArray(list))
+        {
+          console.error("検索結果が配列ではありません:", list);
+          document.getElementById("LblNoResult").style.display = "block";
+          return this;
+        }
+        
+        if(list.length === 0)
+        {
+          document.getElementById("LblNoResult").style.display = "block";
+          return this;
+        }
+        
+        document.getElementById("LblNoResult").style.display = "none";
+        
+        list.forEach(p => {
+
+          let displayImage;
+
+          switch(normal_or_shiny)
+          {
+            case 'NORMAL':
+              displayImage = p.PATH_NORMAL_FRONT;
+              break;
+            case 'SHINY':
+              displayImage = p.PATH_SHINY_FRONT;
+              break;
+            default:
+              displayImage = p.PATH_NORMAL_FRONT;
+              break;
+          }
+          const tr1 = document.createElement("tr");
+          tr1.innerHTML =
+          `
+          <td width="5%" rowspan="2"><img src="../${displayImage}" class="middle-each-image"></td>
+          <td width="5%" rowspan="2">${p.no}</td>
+          <td width="15%">${p.namae}</td>
+          <td width="30%" colspan="3">${p.sugata ? p.sugata : ""}</td>
+          <td width="15%">${p.tamago_group1}${p.tamago_group2 ? "・" + p.tamago_group2 : ""}</td>
+          <td width="10%">${p.chiho}</td>
+          <td width="5%">${p.hp}</td>
+          <td width="5%">${p.attack}</td>
+          <td width="5%">${p.defense}</td>
+          <td width="5%"><a href="../NewPokedex/NewPokedex.html?poke_AUTONUM=${p.AUTONUM}"
+                            data-lang="${jap_or_eng}"
+                            data-window="${bright_or_dark}"
+                            >リンク</a></td>
           `;
           const tr2 = document.createElement("tr");
           tr2.innerHTML =
@@ -380,14 +620,14 @@ const PokeSearch = (() => {
         
         switch(bright_or_dark)
         {
-          case "bright":
+          case 'BRIGHT':
             htmlPageStyle =
             `
             <link rel="stylesheet" href="SearchEggGroupDexBright.css">
             <link rel="icon" href="SearchEggGroupDexBright.png">
             `;
             break;
-          case "dark":
+          case 'DARK':
             htmlPageStyle =
             `
             <link rel="stylesheet" href="SearchEggGroupDexDark.css">
@@ -418,8 +658,20 @@ const PokeSearch = (() => {
       .init()
       .makeFieldPageTitle()
       .makeSettingCommands()
-      .makeDecisionButton()
-      .makeSidebarArea()
+      .makeDecisionButton();
+    switch(jap_or_eng)
+    {
+      case 'JAP':
+        func.makeJapSidebarArea();
+        break;
+      case 'ENG':
+        func.makeEngSidebarArea();
+        break;
+      default:
+        func.makeJapSidebarArea();
+        break;
+    }
+    func
       .bindMenuButton()
       .bindSidebarCloseButton()
       .bindSidebarEvents()
