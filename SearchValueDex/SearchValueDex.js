75efba2dcc02d6154394eda7a0e4ea6121c82b0b
argosdidit5963 <120153198+argosdidit@users.noreply.github.com>
2026-03-20 20:02:20 +0900

    Update SearchValueDex.js

--- SearchValueDex/SearchValueDex.js
+++ SearchValueDex/SearchValueDex.js
@@ -1119,15 +1119,15 @@ const PokeSearch = (() => {
         switch(ascending_or_descending)
         {
           case 'ASCENDING':
-            list.sort((a, b) => a.AUTONUM - b.AUTONUM);
+            list.sort((a, b) => a.autonum - b.autonum);
             break;
 
           case 'DESCENDING':
-            list.sort((a, b) => b.AUTONUM - a.AUTONUM);
+            list.sort((a, b) => b.autonum - a.autonum);
             break;
 
           default:
-            list.sort((a, b) => a.AUTONUM - b.AUTONUM);
+            list.sort((a, b) => a.autonum - b.autonum);
             break;
         }
         
@@ -1254,15 +1254,15 @@ const PokeSearch = (() => {
         switch(ascending_or_descending)
         {
           case 'ASCENDING':
-            list.sort((a, b) => a.AUTONUM - b.AUTONUM);
+            list.sort((a, b) => a.autonum - b.autonum);
             break;
 
           case 'DESCENDING':
-            list.sort((a, b) => b.AUTONUM - a.AUTONUM);
+            list.sort((a, b) => b.autonum - a.autonum);
             break;
 
           default:
-            list.sort((a, b) => a.AUTONUM - b.AUTONUM);
+            list.sort((a, b) => a.autonum - b.autonum);
             break;
         }
         
