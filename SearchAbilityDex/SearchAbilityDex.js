3979aa6512f19e389c3db9e892a6d77b1f706619
argosdidit5963 <120153198+argosdidit@users.noreply.github.com>
2026-03-20 19:56:35 +0900

    Update SearchAbilityDex.js

--- SearchAbilityDex/SearchAbilityDex.js
+++ SearchAbilityDex/SearchAbilityDex.js
@@ -726,15 +726,15 @@ const PokeSearch = (() => {
         switch(ascending_or_descending)
         {
           case 'ASCENDING':
-            list.sort((a, b) => a.AUTONUM - b.AUTONUM);
+            list.sort((a, b) => a.autonum - b.autonum);
             break;
 
           case 'DESCENDING':
-            list.sort((a, b) => b.AUTONUM - a.AUTONUM);
+            list.sort((a, b) => b.autonum - a.autonum);
             break;
 
           default:
-            list.sort((a, b) => a.AUTONUM - b.AUTONUM);
+            list.sort((a, b) => a.autonum - b.autonum);
             break;
         }
         
@@ -861,15 +861,15 @@ const PokeSearch = (() => {
         switch(ascending_or_descending)
         {
           case 'ASCENDING':
-            list.sort((a, b) => a.AUTONUM - b.AUTONUM);
+            list.sort((a, b) => a.autonum - b.autonum);
             break;
 
           case 'DESCENDING':
-            list.sort((a, b) => b.AUTONUM - a.AUTONUM);
+            list.sort((a, b) => b.autonum - a.autonum);
             break;
 
           default:
-            list.sort((a, b) => a.AUTONUM - b.AUTONUM);
+            list.sort((a, b) => a.autonum - b.autonum);
             break;
         }
         
